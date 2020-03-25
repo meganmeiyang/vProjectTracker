17535fe0714adb0b05ffe1b1e9d89b860c8789d0
May <meimeipuppy@gmail.com>
2020-03-25 11:26:52 +0800

    add beforeSave logics to _form
    add before save to calculate total sales and weighed sales before insert or update.

--- controllers/ProjectController.php
+++ controllers/ProjectController.php
@@ -84,8 +84,7 @@ class ProjectController extends Controller
 			
 			//created on & created by
 			
-			$model->created_by = Yii::$app->user->identity->username;
-			$model->created_on = date("Y-m-d H:i:s");
+			
 			if($model->save()) {
 			
            		return $this->redirect(['view', 'id' => $model->id_project]);
@@ -131,6 +130,27 @@ class ProjectController extends Controller
         return $this->redirect(['index']);
     }
 
+    public function beforeSave($insert)
+    {
+        if (!parent::beforeSave($insert)) {
+            return false;
+    }
+
+        // ...custom code here...
+	
+	if($insert) 
+	{
+	    $model->created_by = Yii::$app->user->identity->username;
+	    $model->created_on = date("Y-m-d H:i:s");    
+	}
+	//$newattributes = $this->getAttributes();
+        //$oldattributes = $this->_oldattributes;  
+	$this->sales_total_exp = $this->sales_exp_sim + $this->sales_exp_data + $this->sales_exp_platform + $this->sales_exp_software;
+	$this->sales_total_act = $this->sales_act_sim + $this->sales_act_data + $this->sales_act_platform + $this->sales_act_software;	
+        $this->sales_weighed_total = $this->sales_total_act * $this.getProgressx()->value_progress;
+	return true;
+    }	
+	
     /**
      * Finds the Project model based on its primary key value.
      * If the model is not found, a 404 HTTP exception will be thrown.
